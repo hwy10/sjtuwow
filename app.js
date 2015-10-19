ff83e8e1a65111a89d84ec50549018545fe9f2a8
wing <gzygzy9211@gmail.com>
2015-10-20 01:23:46 +0800

    Battle Net Oauth Works!

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
 
+var BnetStrategy = require('passport-bnet').Strategy;
+var passport = require('passport')
+var oauth2 = require('./.vscode/oauth2.js');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,29 +32,65 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true,
 	cookie: {maxAge:7*24*60*60*1000},
-}))
+}));
+
+passport.serializeUser(function(user, done) { 
+    done(null, user); 
+}); 
+ 
+passport.deserializeUser(function(obj, done) { 
+    done(null, obj); 
+});   
+
+passport.use(new BnetStrategy({
+    clientID: oauth2.BN_KEY,
+    clientSecret: oauth2.BN_SECRET,
+    scope: "wow.profile",
+    region: "cn",
+    callbackURL: "https://sjtuwow.azurewebsites.net/login/callback"
+}, function(accessToken, refreshToken, profile, done) {
+    //console.log("refresh Token = " + refreshToken);
+    //console.log("access Token = " + accessToken);
+    profile.accessToken = accessToken;
+    var str = JSON.stringify(profile);
+    //console.log(str);
+    return done(null, str);
+    //process.nextTick(function () { 
+    //     return done(null, profile); 
+    //   }); 
+}));
+
+console.log(oauth2.BN_KEY);
+console.log(oauth2.BN_SECRET);
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+app.get('/login',  passport.authenticate('bnet'));
+app.get('/login/callback', 
+        passport.authenticate('bnet', { /*successRedirect: '/login/success',*/ failureRedirect: '/' }),
+        function(req, res){ 
+          req.user = JSON.parse(req.user);
+          req.session.passport.user = JSON.parse(req.session.passport.user);
+          //console.log(require('util').inspect(req.session));
+          //console.log(require('util').inspect(req.user));
+          res.send("login!\n" + req.session.passport.battletage);
+        }
+); 
+//app.get('/login/success', function(req, res) { res.send("login!\n" ); });
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+})
+
+
+
 
 app.use(function (req, res, next) {
-    var url = req.originalUrl;
-	if(req.session.user)
-	{
-		var connection = require("./database/db");
-		connection.query("SELECT * FROM user WHERE id=?",[req.session.user.id],function(err,rows,fields){
-			if(!err)
-			{
-				req.session.user = {
-						username : rows[0].username,
-						name : rows[0].name,
-						email : rows[0].email,
-						id : rows[0].id}
-			}
-		})
-		res.locals.user = req.session.user;
-	}
-    else if (!url.startsWith("/users/login") && !url.startsWith("/users/register") && !url.startsWith("/static/")) {
-		return res.redirect("/users/login");
-	}
-    next();
+  console.log(req.url);
+  console.log(" session = " + require('util').inspect( req.session));
+  console.log(" user = " + require('util').inspect( req.user));
+  next();
 });
 
 app.use('/', routes);
