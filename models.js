ffd5987ac3596a575847c4a26441d9a5940caad5
wing <gzygzy9211@gmail.com>
2015-10-20 16:34:09 +0800

    pre merge NO.1

---
+++ models.js
@@ -0,0 +1,113 @@
+var settings = require('./settings');
+var Sequelize = require('sequelize');
+// Init Database
+var db = new Sequelize(settings.DB_NAME, settings.DB_USERNAME, settings.DB_PASSWORD, {
+  host: settings.DB_HOST,
+  port: settings.DB_PORT,
+  dialect: settings.DB_DIALECT,
+  timezone: settings.DB_TIMEZONE
+});
+exports.db = db;
+
+// TODO
+// Add fields needed for BattleNetAPI @GanZhenye
+// And it seems UserGroup is needed since the event should be send to a group of users instead of one by one.
+var User = db.define('User', {
+  uuid: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  }
+}, {
+  underscored: true,
+  tableName: 'user'
+});
+exports.User = User;
+
+var Notice = db.define('Notice', {
+  authorId: {
+    field: 'author_id',
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    defaultValue: ''
+  }
+}, {
+  underscored: true,
+  tableName: 'notice'
+});
+exports.Notice = Notice;
+
+var Event = db.define('Event', {
+  launcherId: {
+    field: 'launcher_id',
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  type: {
+    type: Sequelize.ENUM,
+    values: ['ONLINE', 'OFFLINE'],
+    allowNull: false,
+  },
+  startAt: {
+    field: 'start_at',
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  address: {
+    type: Sequelize.STRING,
+    defaultValue: ''
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    defaultValue: ''
+  },
+}, {
+  underscored: true,
+  tableName: 'event'
+});
+exports.Event = Event;
+
+var React = db.define('React', {
+  eventId: {
+    field: 'event_id',
+    type: Sequelize.INTEGER.UNSIGNED,
+    allowNull: false
+  },
+  participantId: {
+    field: 'participant_id',
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  wowRole: {
+    field: 'wow_role',
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  replyStatus: {
+    field: 'replay_status',
+    type: Sequelize.ENUM,
+    values: ['PENDING', 'ACCEPT', 'DENY', 'DELAY'],
+    allowNull: false
+  },
+  replyAt: {
+    field: 'reply_at',
+    type: Sequelize.DATE
+  },
+  replyContent: {
+    field: 'replay_content',
+    type: Sequelize.STRING,
+    defaultValue: ''
+  }
+}, {
+  underscored: true,
+  tableName: 'react'
+});
+exports.React = React;
---
+++ settings.js
@@ -0,0 +1,57 @@
+// Global settings for the project
+
+/*
+ * The following constants are not suggested to be changed.
+ */
+var path = require('path');
+var nconf = require('nconf');
+nconf.file(path.join(__dirname, 'config.json'));
+
+// MD5 of "a website for sjtuwower"
+exports.SECRET_KEY = 'E1925844EDED520E2B8B6CCD4F21E911';
+
+exports.DEBUG = true;
+exports.SEVER_PORT = 3000;
+
+exports.STATIC_URL = '/static';
+exports.STATIC_ROOT = path.join(__dirname, 'public');
+
+exports.VIEW_ROOT = path.join(__dirname, 'views');
+
+/*
+ * API Constants
+ */
+exports.API_URL = '/api';
+
+/*
+ * Session Constants
+ */
+exports.SESSION_NAME = 'sjtuwow';
+
+/*
+ * Cookie timeout
+ */
+exports.COOKIE_TIMEOUT = 7 * 24 * 60 * 60 * 1000;
+
+/*
+ * The following constants should be changed in your own config.json at the root of the project.
+ * Example of the config.json:
+ * {
+ *   "DB_NAME" : "sjtuwow",
+ *   "DB_USERNAME" : "root",
+ *   "DB_PASSWORD" : "123456",
+ *   "BN_KEY": "xxxxx",
+ *   "BN_SECRET": "xxxxx",
+ * }
+ */
+
+/*
+ * DB Constants
+ */
+exports.DB_HOST = nconf.get('DB_HOST') ? nconf.get('DB_HOST') : '127.0.0.1';
+exports.DB_PORT = nconf.get('DB_PORT') ? nconf.get('DB_PORT') : '3306';
+exports.DB_DIALECT = nconf.get('DB_DIALECT') ? nconf.get('DB_DIALECT') : 'mysql';
+exports.DB_NAME = nconf.get('DB_NAME') ? nconf.get('DB_NAME') : '';
+exports.DB_USERNAME = nconf.get('DB_USERNAME') ? nconf.get('DB_USERNAME') : '';
+exports.DB_PASSWORD = nconf.get('DB_PASSWORD') ? nconf.get('DB_PASSWORD') : '';
+exports.DB_TIMEZONE = nconf.get('DB_TIMEZONE') ? nconf.get('DB_TIMEZONE') : '+00:00';
